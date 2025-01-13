ea373127f0662bf8bd0d48b3bb658512d1de0090
VAGNER OLIVEIRA <116770973+wildyhunter@users.noreply.github.com>
2025-01-13 12:27:54 +0000

    create componente filters

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 
 import MovieCard from '../components/MovieCard.jsx';
-import useYearMovies from '../constants/YearMovies.jsx';
+import Filter from '../components/Filters.jsx';
 
 import './Home.css';
 
@@ -17,26 +17,13 @@ const categories = [
     'Horror',
     'Sci-Fi',
 ];
-// const years = ['2024', '2023', '2022', '2021', '2020'];
-
-const selectedStyle = {
-    backgroundColor: '#FC6B01',
-    color: '#000',
-    border: 'none',
-};
-
-const defaultStyle = {
-    backgroundColor: '#2c2c2c',
-    color: '#fff',
-    border: 'none',
-};
+const years = ['2024', '2023', '2022', '2021', '2020'];
 
 const Home = () => {
     const [topMovies, setTopMovies] = useState([]);
     const [selected, setSelected] = useState(0);
     const [categorySelected, setCategorySelected] = useState('');
     const [yearSelected, setYearSelected] = useState('');
-    const years = useYearMovies();
 
     const getTopRatedMovies = async (url) => {
         const res = await fetch(url);
@@ -51,73 +38,18 @@ const Home = () => {
 
     return (
         <div className="homeContainer">
-            <div className="title">
-                <h1>Movie ranking TOP 10 </h1>
-            </div>
             <section className="filterContainer">
-                <div className="filter">
-                    {buttons.map((btn, index) => (
-                        <button
-                            key={index}
-                            onClick={() => {
-                                setSelected(index);
-                                setYearSelected('');
-                                setCategorySelected('');
-                            }}
-                            className={selected === index ? 'selected' : ''}
-                            style={{
-                                backgroundColor:
-                                    selected === index ? '#FC6B01' : '#2c2c2c',
-                                color: selected === index ? '#000' : '#fff',
-                            }}
-                        >
-                            {btn}
-                        </button>
-                    ))}
-
-                    <select
-                        name="category"
-                        id="category"
-                        value={categorySelected}
-                        onChange={(e) => {
-                            setCategorySelected(e.target.value);
-                            setSelected(null);
-                            setYearSelected(null);
-                        }}
-                        style={{
-                            ...(categorySelected
-                                ? selectedStyle
-                                : defaultStyle),
-                        }}
-                    >
-                        {categories.map((cat, index) => (
-                            <option key={index} value={cat}>
-                                {cat}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-                <div className="filterYear filter">
-                    <select
-                        name="year"
-                        id="year"
-                        value={yearSelected}
-                        onChange={(e) => {
-                            setYearSelected(e.target.value);
-                            setSelected(null);
-                            setCategorySelected(null);
-                        }}
-                        style={{
-                            ...(yearSelected ? selectedStyle : defaultStyle),
-                        }}
-                    >
-                        {years.map((year, index) => (
-                            <option key={index} value={year}>
-                                {year}
-                            </option>
-                        ))}
-                    </select>
-                </div>
+                <Filter
+                    buttons={buttons}
+                    categories={categories}
+                    years={years}
+                    selected={selected}
+                    setSelected={setSelected}
+                    categorySelected={categorySelected}
+                    setCategorySelected={setCategorySelected}
+                    yearSelected={yearSelected}
+                    setYearSelected={setYearSelected}
+                />
             </section>
             <div className="moviesContainer">
                 {topMovies.length === 0 && (
